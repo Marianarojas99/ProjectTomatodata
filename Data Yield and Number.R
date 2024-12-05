d81295d410716dac9dd51ffff68439c0a0cd71f2
Mariana Rojas <marianarojas99@gmail.com>
2024-12-05 15:20:56 +0100

    Table Yield and number finished

--- Data Yield and Number.R
+++ Data Yield and Number.R
@@ -22,26 +22,44 @@ View(Dfprov2)
 colnames(Dfprov2) <- c("Treatment","Repetition","Cluster1","Cluster2","Cluster3",
                        "Cluster4","Cluster5", "Cluster6","Cluster1N","Cluster2N","Cluster3N",
                        "Cluster4N","Cluster5N", "Cluster6N")
+View(Dfprov2)
+
 
 #Call the libr
 library(dplyr)
 library(tidyr)
 
 # Transforming Data (Long Table)
-dflong <- Dfprov2 %>%
+df_yield <- Dfprov2 %>%
   pivot_longer(cols = c(Cluster1,Cluster2,Cluster3,
                         Cluster4,Cluster5, Cluster6),
                names_to = "Clusters",
                values_to = "Yield")
-View(dflong)
+dfjustyield <- df_yield[,-c(3:8)]
+View(dfjustyield)
 
-dflongcomp <- dflong %>%
-  pivot_longer(cols = c(Cluster1N,Cluster2N,Cluster3N,
-                        Cluster4N,Cluster5N, Cluster6N),
-               names_to = "ClustersN",
-               values_to = "Number of Fruits")
 
-View(dflongcomp)
+df_fruits <- Dfprov2 %>%
+  pivot_longer(
+    cols = c(Cluster1N, Cluster2N, Cluster3N, Cluster4N, Cluster5N, Cluster6N),
+    names_to = "Clusters",
+    values_to = "Number_of_Fruits"
+  ) %>%
+  mutate(
+    Clusters = gsub("N$", "", Clusters) 
+  )
+View(df_fruits)
+dfjustfruit <- df_fruits[,-c(3:8)]
+View(dfjustfruit)
+dfjustfruit <- dfjustfruit %>% group_by(Treatment, Repetition, Clusters) %>% slice(1) %>% ungroup()
+dfjustyield <- dfjustyield %>% group_by(Treatment, Repetition, Clusters) %>% slice(1) %>% ungroup()
+df_long <- left_join(dfjustfruit, dfjustyield, by = c("Treatment", "Repetition", "Clusters"))
+View(df_long)
 
-dffinal <- dflongcomp [,-5]
-View(dffinal)
+Bloc <- rep(1:5, each = 18)
+df_long$Blocks <- Bloc
+View(df_long)
+
+df_long <- df_long %>%
+  select(1:2, 6, 3:5, everything())
+View(df_long)
